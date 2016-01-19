b891a8fe80837ab1bf70aba59e2b231766e1122b
KASHIMA Kazuo <k4200@kazu.tv>
2016-01-19 01:56:15 -0800

    Implement CRUD; chane route

--- app/controllers/webhook_settings_controller.rb
+++ app/controllers/webhook_settings_controller.rb
@@ -1,6 +1,8 @@
 class WebhookSettingsController < ApplicationController
   unloadable
 
+  #before_filter :find_project, :except => [:show, :edit, :update, :destroy]
+  #before_filter :authorize
   before_filter :find_project, :authorize
 
   def index
@@ -11,20 +13,34 @@ class WebhookSettingsController < ApplicationController
   end
 
   def create
+    @webhook_setting = WebhookSetting.new(webhook_setting_params)
+    @webhook_setting.project_id = @project.id
+    @webhook_setting.save
+    redirect_to [@project, @webhook_setting]
   end
 
   def show
-    webhook_setting = WebhookSetting.find(params[:id])
+    @webhook_setting = WebhookSetting.find(params[:id])
   end
 
   def edit
-    webhook_setting = WebhookSetting.find(params[:id])
+    @webhook_setting = WebhookSetting.find(params[:id])
   end
 
   def update
+    @webhook_setting = WebhookSetting.find(params[:id])
+    if @webhook_setting.update(webhook_setting_params)
+      redirect_to [@project, @webhook_setting]
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @webhook_setting = WebhookSetting.find(params[:id])
+    @webhook_setting.destroy
+
+    redirect_to project_webhook_settings_path
   end
 
   private
@@ -33,5 +49,10 @@ class WebhookSettingsController < ApplicationController
     @project = Project.find(params[:project_id])
   rescue ActiveRecord::RecordNotFound
     render_404
+  end
+
+  def webhook_setting_params
+    params.require(:webhook_setting)
+    .permit(:url, :description, :send_issue, :send_wiki, :send_document, :send_file)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  resources :webhook_settings, :only => [:show, :edit, :update, :destroy]
-
   resources :projects do
-    resources :webhook_settings, :except => [:show, :edit, :update, :destroy]
+    resources :webhook_settings
   end
 end
